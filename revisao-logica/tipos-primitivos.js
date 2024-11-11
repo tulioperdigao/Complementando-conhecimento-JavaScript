db04af906043761f35771bbdaa8fd5d525aeb89b
tulioperdigao <116309232+tulioperdigao@users.noreply.github.com>
2024-11-11 16:01:38 -0300

    Create tipos-primitivos.js

---
+++ revisao-logica/tipos-primitivos.js
@@ -0,0 +1,10 @@
+//strings, number (int, float), boolean
+
+let string = "minha string - aspas 'simples'"
+let string1 = 'minha string - aspas "duplas"'
+let string2 = `minha template literal: ${string1}`
+
+const n1 = 10
+const n2 = 1.1
+console.log(`O tipo de n1 é ${typeof n1} e seu valor é ${n1}`)
+console.log(`O tipo de n1 é ${typeof n2} e seu valor é ${n2}`)
