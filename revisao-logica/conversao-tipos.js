fbda187673166ddcf5982d0caa37ac0363306896
tulioperdigao <116309232+tulioperdigao@users.noreply.github.com>
2024-11-12 14:25:17 -0300

    revisao-conversao-tipos

---
+++ revisao-logica/conversao-tipos.js
@@ -0,0 +1,14 @@
+let n1 = 10
+//n1 = parseFloat(n1)
+
+let n2= 2
+
+console.log(n1 * n2, typeof n1, typeof n2, n1)
+
+//parseInt, parseFloar, Number()
+
+
+
+n2 = 12
+n2 = n2.toString(2)
+console.log(n2, typeof n2)
