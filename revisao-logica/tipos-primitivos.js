dfbd134697a076b4c9da76ddefa95b70c9bf4c64
tulioperdigao <116309232+tulioperdigao@users.noreply.github.com>
2024-11-12 13:48:56 -0300

    revisao-tipos-primitivos

--- revisao-logica/tipos-primitivos.js
+++ revisao-logica/tipos-primitivos.js
@@ -1,4 +1,5 @@
-//strings, number (int, float), boolean
+// strings, number (int, float), boolean
+// undefined, null, symbol (ES2015)
 
 let string = "minha string - aspas 'simples'"
 let string1 = 'minha string - aspas "duplas"'
@@ -8,3 +9,13 @@ const n1 = 10
 const n2 = 1.1
 console.log(`O tipo de n1 é ${typeof n1} e seu valor é ${n1}`)
 console.log(`O tipo de n1 é ${typeof n2} e seu valor é ${n2}`)
+
+const isValid = true
+console.log(`isValid: ${isValid}`)
+console.log(`Tipo: ${typeof isValid}`)
+
+let varTeste = null
+console.log(varTeste)
+console.log(typeof varTeste)
+varTeste = 10
+console.log(typeof varTeste, varTeste)
